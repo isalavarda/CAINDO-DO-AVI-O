39b6286f02278df4989de325529c8547eb1367fa
isalavarda <isabelly.lavarda@escola.pr.gov.br>
2025-08-19 15:03:23 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+const avanca = document.querySelectorAll('.btn-proximo');
+
+
+avanca.forEach(button => {
+   button.addEventListener('click', function(){
+       const atual = document.querySelector('.ativo');
+       const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+
+       atual.classList.remove('ativo');
+       document.getElementById(proximoPasso).classList.add('ativo');
+   })
+})
+body {
+   background-color: #1D4221;
+   color: white;
+   font-family: "Bai Jamjuree", sans-serif;
+   display: flex;
+   justify-content: center;
+   align-items: center;
